8540022d974a00ea0d13a8188b54f3579d105368
yassuhacks750 <yassudj8085231272@gmail.com>
2025-07-27 23:53:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,10 +56,10 @@ api_url = "http://master-api-v3.vercel.app/"
 api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
 token_cp ='eyJjb3Vyc2VJZCI6IjQ1NjY4NyIsInR1dG9ySWQiOm51bGwsIm9yZ0lkIjo0ODA2MTksImNhdGVnb3J5SWQiOm51bGx9r'
 adda_token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkcGthNTQ3MEBnbWFpbC5jb20iLCJhdWQiOiIxNzg2OTYwNSIsImlhdCI6MTc0NDk0NDQ2NCwiaXNzIjoiYWRkYTI0Ny5jb20iLCJuYW1lIjoiZHBrYSIsImVtYWlsIjoiZHBrYTU0NzBAZ21haWwuY29tIiwicGhvbmUiOiI3MzUyNDA0MTc2IiwidXNlcklkIjoiYWRkYS52MS41NzMyNmRmODVkZDkxZDRiNDkxN2FiZDExN2IwN2ZjOCIsImxvZ2luQXBpVmVyc2lvbiI6MX0.0QOuYFMkCEdVmwMVIPeETa6Kxr70zEslWOIAfC_ylhbku76nDcaBoNVvqN4HivWNwlyT0jkUKjWxZ8AbdorMLg"
-photologo = 'https://ibb.co/4RgQKHXq' #https://envs.sh/GV0.jpg
-photoyt = 'https://ibb.co/4RgQKHXq' #https://envs.sh/GVi.jpg
-photocp = 'https://ibb.co/4RgQKHXq'
-photozip = 'https://ibb.co/4RgQKHXq'
+photologo = 'https://d.img.vision/yashthakurbot/1000600317.png' #https://envs.sh/GV0.jpg
+photoyt = 'https://d.img.vision/yashthakurbot/1000600317.png' #https://envs.sh/GVi.jpg
+photocp = 'https://d.img.vision/yashthakurbot/1000600317.png'
+photozip = 'https://d.img.vision/yashthakurbot/1000600317.png'
 
 
 # Inline keyboard for start command
@@ -72,8 +72,8 @@ keyboard = InlineKeyboardMarkup(
 
 # Image URLs for the random image feature
 image_urls = [
-    "https://tinypic.host/images/2025/02/07/IMG_20250207_224444_975.jpg",
-    "https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png",
+    "https://d.img.vision/yashthakurbot/1000600317.png",
+    "https://d.img.vision/yashthakurbot/1000600317.png",
     # Add more image URLs as needed
 ]
 
